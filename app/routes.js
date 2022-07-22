6441cf37244e7eaae53c2106a2946e85c77536d6
tarikjohnston1 <108064159+tarikjohnston2@users.noreply.github.com>
2022-07-22 16:47:04 +0100

    Update to routes file

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import { exampleWizardRoutes } from './routes/example-wizard.js'
+import { changeDetailsRoutes } from './routes/change-details.js'
 
 const router = express.Router()
 exampleWizardRoutes(router)
