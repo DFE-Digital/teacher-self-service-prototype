00ebb85e14e3eec22882b613cc638f100362f8ca
tarikjohnston1 <108064159+tarikjohnston2@users.noreply.github.com>
2022-12-16 16:28:53 +0000

    Update change-details.js

--- app/wizards/change-details.js
+++ app/wizards/change-details.js
@@ -3,7 +3,7 @@ import { wizard } from 'govuk-prototype-rig'
 export function changeDetails (req) {
   const journey = {
     '/change-name/enter-name': {},
-    '/change-name/confirmation': {}
+    '/change-name/cnfirmation': {}
   }
 
   return wizard(journey, req)
